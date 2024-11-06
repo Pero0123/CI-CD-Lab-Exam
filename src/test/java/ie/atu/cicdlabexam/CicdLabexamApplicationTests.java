b8be178b845ea12c872fc650205ba5a1a1ef9a94
G00422221@atu.ie <G00422221@atu.ie>
2024-11-06 14:12:16 +0000

    created Project

---
+++ src/test/java/ie/atu/cicdlabexam/CicdLabexamApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlabexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLabexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
